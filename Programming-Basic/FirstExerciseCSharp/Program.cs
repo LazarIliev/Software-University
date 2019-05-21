9376dcd3188aee06ce3467ccfa900124ab83764d
Lazar <lazarilievplovdiv@mail.bg>
2019-05-21 15:30:51 +0300

    my first

---
+++ Programming-Basic/FirstExerciseCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstExerciseCSharp
+{
+    class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}",new string('*',n));
+
+            for (int row = 0; row <n-2; row++)
+            {
+                Console.WriteLine("*{0}*",new string(' ',n-2));
+            }
+
+            Console.WriteLine("{0}",new string('*',n));
+        }
+    }
+}
