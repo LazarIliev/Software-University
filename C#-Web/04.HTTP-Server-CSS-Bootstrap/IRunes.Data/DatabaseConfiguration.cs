224e1865c11e1dffdf5e4293950c3e06b9a4d49a
Lazar <lazarilievplovdiv@mail.bg>
2019-06-03 12:26:56 +0300

    http server css bootstrap

---
+++ C#-Web/04.HTTP-Server-CSS-Bootstrap/IRunes.Data/DatabaseConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IRunes.Data
+{
+    public class DatabaseConfiguration
+    {
+        
+            public const string ConnectionString = "Server=DESKTOP-GNLE21C\\SQLEXPRESS;Database=IRuneDB;Integrated Security=True;Trusted_Connection=True";
+        
+    }
+}
